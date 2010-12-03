164d20fd568cf808b7076d0649f97590a26bd6ee
Yutaka HARA <yutaka.hara+github@gmail.com>
2010-12-03 16:10:02 +0900

    refactor: moved view logic of compile to helper

--- app/controllers/compile_controller.rb
+++ app/controllers/compile_controller.rb
@@ -2,6 +2,7 @@ require 'pp'
 
 class CompileController < ApplicationController
   def index
+    @insns = Insns
     @src = params[:src] || "1+2"
 
     if @src.length > 10000
@@ -14,38 +15,6 @@ class CompileController < ApplicationController
 
   private
   
-  include Rack::Utils
-  alias h escape_html
-
-  def highlight_insn(html)
-    html.gsub(/#{Insns.map{|i| ":#{i.name}"}.join("|")}/){|match|
-      name = match.sub(/:/, "")
-      "<a href='/##{h name}'>:#{h name}</a>"
-    }
-  end
-
-  def highlight_opt_insn(html)
-    html.gsub(/:opt_\w+/){|match|
-      "<span class='opt_insn'>#{h match}</span>"
-    }
-  end
-
-  def highlight_insn_in_disasm(html)
-    html.gsub(/#{Insns.map{|i| "\\d+ #{i.name}"}.join("|")}/){|match|
-      match =~ /(\d+) (.*)/
-      addr, name = $1, $2
-      if name =~ /\Aopt_/
-        "#{h addr} <a href='/##{h name}' class='opt_insn'>#{h name}</a>"
-      else
-        "#{h addr} <a href='/##{h name}'>#{h name}</a>"
-      end
-    }
-  end
-
-  def prettify_disasm(disasm)
-    highlight_insn disasm.gsub(/^== disasm/, "\n== disasm")
-  end
-
   OPTIMIZATIONS = [
     :inline_const_cache       ,
     :peephole_optimization    ,
@@ -58,22 +27,15 @@ class CompileController < ApplicationController
   ]
   def compile(src, optimize)
     if optimize
-      iseq = RubyVM::InstructionSequence.compile(src)
+      RubyVM::InstructionSequence.compile(src)
     else
       opt = Hash[*OPTIMIZATIONS.map{|n| [n, false]}.flatten]
       with_compile_opt(opt) do
-        iseq = RubyVM::InstructionSequence.compile(src)
+        RubyVM::InstructionSequence.compile(src)
       end
     end
-
-    html = h iseq.to_a.pretty_inspect
-    tree = highlight_opt_insn highlight_insn highlight_opt_insn html
-
-    asm = highlight_insn_in_disasm prettify_disasm iseq.disasm
-
-    tree + "\n" + asm
   rescue SyntaxError
-    "compile error"
+    nil
   end
 
   def with_compile_opt(opt)
--- app/helpers/compile_helper.rb
+++ app/helpers/compile_helper.rb
@@ -1,2 +1,47 @@
 module CompileHelper
+  def tree(iseq)
+    if iseq
+      highlight_opt_insn highlight_insn h(iseq.to_a.pretty_inspect)
+    else
+      "compile error"
+    end
+  end
+
+  def disasm(iseq)
+    if iseq
+      highlight_insn_in_disasm prettify_disasm iseq.disasm
+    end
+  end
+
+  private
+
+  def highlight_insn(html)
+    html.gsub(/#{@insns.map{|i| ":#{i.name}"}.join("|")}/){|match|
+      name = match.sub(/:/, "")
+      "<a href='/##{h name}'>:#{h name}</a>"
+    }
+  end
+
+  def highlight_opt_insn(html)
+    html.gsub(/:opt_\w+/){|match|
+      "<span class='opt_insn'>#{h match}</span>"
+    }
+  end
+
+  def highlight_insn_in_disasm(html)
+    html.gsub(/#{@insns.map{|i| "\\d+ #{i.name}"}.join("|")}/){|match|
+      match =~ /(\d+) (.*)/
+      addr, name = $1, $2
+      if name =~ /\Aopt_/
+        "#{h addr} <a href='/##{h name}' class='opt_insn'>#{h name}</a>"
+      else
+        "#{h addr} <a href='/##{h name}'>#{h name}</a>"
+      end
+    }
+  end
+
+  def prettify_disasm(disasm)
+    highlight_insn disasm.gsub(/^== disasm/, "\n== disasm")
+  end
+
 end

bd221361b3fcf2890d557b255a85af52e73f094e
Yutaka HARA <yutaka.hara+github@gmail.com>
2010-12-03 16:01:39 +0900

    highlight variables in C code (closes #7)

--- app/helpers/insns_helper.rb
+++ app/helpers/insns_helper.rb
@@ -1,5 +1,27 @@
 module InsnsHelper
-  def wrap_ccode(ccode)
-    "{\n" + ccode + "}"
+  def highlighted_in
+  end
+
+  def highlighted_out
+  end
+
+  def highlight_vars(insn, txt)
+    invars = insn.pops.map &:last
+    operands = insn.opes.map &:last
+    outvars = insn.rets.map &:last
+
+    txt = h txt
+    {
+      invars => "invar",
+      operands => "operand",
+      outvars => "outvar",
+    }.each do |names, css|
+      names.each do |v|
+        next if v == "..."
+        txt.gsub!(/\b#{v}\b/){ "<span class='#{css}'>#{v}</span>" }
+      end
+    end
+
+    txt
   end
 end
